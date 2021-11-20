7a2024d3c3587252d2ce273af7eb6e6827699f4f
Joao Moita Manrique <joaomoitamanrique@gmail.com.br>
2021-11-19 21:49:07 -0300

    busca variavel do node

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+interface HomeProps {
+  envVar: string;
+}
+
+const Home: NextPage<HomeProps> = ({ envVar }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,7 +21,7 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
         <p className={styles.description}>
-          Process ENV {process.env.ENV_VAR}
+          Process ENV {envVar}
         </p>
 
         <p className={styles.description}>
@@ -73,3 +77,13 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const envVar = process.env.ENV_VAR ?? 'FAIL'
+  return {
+    props: {
+      envVar
+    },
+  }
+}
