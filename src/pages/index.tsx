9e6258803ea2a6718928e7b35ddbdb26adca24b1
Joao Moita Manrique <joaomoitamanrique@gmail.com.br>
2021-11-19 21:34:27 -0300

    altera schema aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,9 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <p className={styles.description}>
+          {process.env.ENV_VAR}
+        </p>
 
         <p className={styles.description}>
           Get started by editing{' '}
